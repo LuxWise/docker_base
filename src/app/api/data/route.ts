4f0250c98b94b2352b1a0341c0880ba6203f941a
LuxWise <jcsanchez.martinez.2020@gmail.com>
2025-05-24 00:59:13 -0500

    feat: add page 'felicidades' and refactor api/data

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -2,7 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  console.log(body);
+
+  await fetch(
+    `http://localhost:8084/mail/send?email=jcsanchez.martinez.2020@gmail.com`,
+    {
+      method: "GET",
+    }
+  );
 
   return NextResponse.json({ data: body }, { status: 200 });
 }
--- src/app/felicidades/page.tsx
+++ src/app/felicidades/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import Docker from "@/public/svg/docker-logo-white.svg";
+import Image from "next/image";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="flex flex-col bg-gradient-to-r from-[#1f62ec] to-[#022a8e] items-center gap-10 pt-10 h-screen">
+      <header>
+        <Image src={Docker} alt="" className="max-w-44" />
+      </header>
+      <main className="flex justify-center items-center w-full h-full bg-white ">
+        <div className="flex flex-col bg-[#1d63ed] items-center justify-center w-4/6 h-4/6 text-white px-8 rounded-2xl shadow-2xl">
+          <h1 className="text-4xl font-bold mb-4 text-center">¡Felicidades!</h1>
+          <p className="text-lg text-center mb-2">
+            Has completado esta etapa con éxito. Muy pronto nos volveremos a ver
+            para seguir aprendiendo juntos.
+          </p>
+          <p className="text-lg text-center">
+            Te llegará un correo de confirmación en breve. ¡Gracias por tu
+            dedicación y entusiasmo!
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
